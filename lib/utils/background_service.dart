d302a9cd4bfa52de265f4429db9d1a33cd2a36bf
dayens13 <dionsunardi1999@gmail.com>
2022-03-27 15:53:13 +0700

    membuat background services

---
+++ lib/utils/background_service.dart
@@ -0,0 +1,35 @@
+import 'dart:isolate';
+import 'dart:ui';
+import 'package:flutter_fundamental_3/data/api_service/api_service.dart';
+import 'package:flutter_fundamental_3/utils/notification_helper.dart';
+
+final ReceivePort port = ReceivePort();
+
+class BackgroundService {
+  static BackgroundService? _instance;
+  static String _isolateName = 'isolate';
+  static SendPort? _uiSendPort;
+
+  BackgroundService._internal() {
+    _instance = this;
+  }
+
+  factory BackgroundService() => _instance ?? BackgroundService._internal();
+
+  void initializeIsolate() {
+    IsolateNameServer.registerPortWithName(
+      port.sendPort,
+      _isolateName,
+    );
+  }
+
+  static Future<void> callback() async {
+    print('Alarm fired!');
+    final NotificationHelper _notificationHelper = NotificationHelper();
+    var result = await ApiService().listRestaurant();
+    await _notificationHelper.showNotification(flutterLocalNotificationsPlugin, result);
+
+    _uiSendPort ??= IsolateNameServer.lookupPortByName(_isolateName);
+    _uiSendPort?.send(null);
+  }
+}
