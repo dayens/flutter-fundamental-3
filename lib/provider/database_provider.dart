0dc9127e4571c9d88dbf979c5ebd28efb5ab1037
dayens13 <dionsunardi1999@gmail.com>
2022-03-27 11:52:43 +0700

    membuat database provider

---
+++ lib/provider/database_provider.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fundamental_3/data/db/database_helper.dart';
+import 'package:flutter_fundamental_3/data/model/restaurant.dart';
+import 'package:flutter_fundamental_3/utils/result_state.dart';
+
+class DatabaseProvider extends ChangeNotifier {
+  final DatabaseHelper databaseHelper;
+
+  DatabaseProvider({required this.databaseHelper}) {
+    _getRestaurant();
+  }
+
+  late ResultState _state;
+  ResultState get state => _state;
+
+  String _message = '';
+  String get message => _message;
+
+  List<Restaurant> _restaurant = [];
+  List<Restaurant> get restaurant => _restaurant;
+
+  void _getRestaurant() async {
+    _restaurant = await databaseHelper.getRestaurant();
+
+    if (_restaurant.length > 0) {
+      _state = ResultState.hasData;
+    } else {
+      _state = ResultState.noData;
+      _message = 'Empty Data';
+    }
+    notifyListeners();
+  }
+
+  void addRestaurant(Restaurant restaurant) async {
+    try {
+      await databaseHelper.insertRestaurant(restaurant);
+      _getRestaurant();
+    } catch (e) {
+      _state = ResultState.error;
+      _message = 'Error: $e';
+      notifyListeners();
+    }
+  }
+
+  Future<bool> isBookmarked(String id) async {
+    final bookmarkedRestaurant = await databaseHelper.getRestaurantById(id);
+    return bookmarkedRestaurant.isNotEmpty;
+  }
+
+  void removeRestaurant(String id) async {
+    try {
+      await databaseHelper.removeRestaurant(id);
+      _getRestaurant();
+    } catch (e) {
+      _state = ResultState.error;
+      _message = 'Error: $e';
+      notifyListeners();
+    }
+  }
+}
